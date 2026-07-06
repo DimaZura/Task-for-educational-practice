f51afc203483aa1d2c5f9f93d3c8148e58cbd3db
Dmitry <av1900333388555@gmail.com>
2026-07-06 22:10:37 +0300

    first

---
+++ internal/Graph/Graph.h
@@ -0,0 +1,13 @@
+//
+// Created by mac on 06.07.2026.
+//
+
+#ifndef TASK_FOR_EDUCATIONAL_PRACTICE_GRAPH_H
+#define TASK_FOR_EDUCATIONAL_PRACTICE_GRAPH_H
+
+
+class Graph {
+};
+
+
+#endif //TASK_FOR_EDUCATIONAL_PRACTICE_GRAPH_H
